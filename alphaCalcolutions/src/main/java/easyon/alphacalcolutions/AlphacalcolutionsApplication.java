f8b375c3e22b633309bdbd01ebce711c63f2b875
Joergen Koeldal <joer1894@stud.kea.dk>
2020-11-25 16:32:29 +0100

    created alphacalcolutions folder and bilag

---
+++ alphaCalcolutions/src/main/java/easyon/alphacalcolutions/AlphacalcolutionsApplication.java
@@ -0,0 +1,13 @@
+package easyon.alphacalcolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphacalcolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphacalcolutionsApplication.class, args);
+    }
+
+}
---
+++ alphaCalcolutions/src/test/java/easyon/alphacalcolutions/AlphacalcolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package easyon.alphacalcolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphacalcolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
