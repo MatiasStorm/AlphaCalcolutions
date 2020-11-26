e3fd75239418c253c09c8d09e12cad16dd60cb66
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2020-11-26 13:45:40 +0100

    make get & post mapping of Employee in mainController

--- alphaCalcolutions/src/main/java/easyon/alphacalcolutions/controller/MainController.java
+++ alphaCalcolutions/src/main/java/easyon/alphacalcolutions/controller/MainController.java
@@ -1,7 +1,26 @@
 package easyon.alphacalcolutions.controller;
 
+import easyon.alphacalcolutions.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/createEmployee")
+    public String createUser(Model model, Employee employee) {
+        model.addAttribute("employee", employee);
+        return "createEmployee";
+    }
+
+    @PostMapping("/createEmployee/submit")
+    public String createUserSubmit(Employee employee, Model model, WebRequest request) {
+
+        Employee newEmployee = EmployeeService.createEmployee(employee);
+
+        return "redirect:/index";
+    }
 }
