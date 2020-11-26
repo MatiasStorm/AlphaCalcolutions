6b39aca520a2a2441ea1fd3270ebd94f8cdccf3f
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2020-11-26 13:14:07 +0100

    create Employee model

---
+++ alphaCalcolutions/src/main/java/easyon/alphacalcolutions/model/Employee.java
@@ -0,0 +1,18 @@
+package easyon.alphacalcolutions.model;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int titleId;
+    private int hourlySalary;
+
+    public Employee (String firstName, String lastName, int titleId, int hourlySalary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.titleId = titleId;
+        this.hourlySalary = hourlySalary;
+    }
+
+
+}
