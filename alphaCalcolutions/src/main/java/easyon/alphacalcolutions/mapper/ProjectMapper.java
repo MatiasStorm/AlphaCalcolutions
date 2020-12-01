e26abb559302468ae765dc80c8eb90246386e3a2
MatiasStorm <mat-435@hotmail.com>
2020-12-01 17:39:17 +0100

    Format date before insert

--- alphaCalcolutions/src/main/java/easyon/alphacalcolutions/mapper/ProjectMapper.java
+++ alphaCalcolutions/src/main/java/easyon/alphacalcolutions/mapper/ProjectMapper.java
@@ -4,13 +4,15 @@ import easyon.alphacalcolutions.model.Project;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 
 public class ProjectMapper {
 
-    public Project mapRow(ResultSet rs) throws SQLException {
+    public Project mapRow(ResultSet rs) throws SQLException, ParseException {
         Project project = new Project();
         project.setProjectId(rs.getInt("project_id"));
         project.setTitle(rs.getString("project_title"));
+        project.setStartDate(rs.getString("project_start_date"));
 //        project.setProjectLeader(rs.getInt("project_leader_id"));
 //        project.setAssignedUsers();
 //        project.setDeadline();
--- alphaCalcolutions/src/main/java/easyon/alphacalcolutions/repository/ProjectDAO.java
+++ alphaCalcolutions/src/main/java/easyon/alphacalcolutions/repository/ProjectDAO.java
@@ -5,6 +5,8 @@ import easyon.alphacalcolutions.mapper.ProjectMapper;
 import easyon.alphacalcolutions.model.Project;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class ProjectDAO {
     ProjectMapper projectMapper = new ProjectMapper();
@@ -14,9 +16,11 @@ public class ProjectDAO {
             Connection con = DBManager.getConnection();
             String SQL = "INSERT INTO projects (project_title, project_start_date, project_end_date, project_leader_id) VALUES (?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
             ps.setString(1, project.getTitle());
-            ps.setDate(2, (Date) project.getStartDate());
-            ps.setDate(3, (Date) project.getEndDate());
+            ps.setString(2, dateFormat.format(project.getStartDate()));
+            ps.setString(3, dateFormat.format(project.getEndDate()));
             ps.setInt(4, project.getProjectLeaderId());
             ps.executeUpdate();
             ResultSet ids = ps.getGeneratedKeys();
